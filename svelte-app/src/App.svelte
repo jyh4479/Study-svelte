a4901c59e3c17ec4c2cdf7ee87849815c294ba6a
Jeong yong hoon <jyh.dev@gmail.com>
2021-08-07 16:33:40 +0900

    svelte test

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
 	export let name;
+	export let nickName;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {name} - {nickName}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
